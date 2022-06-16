55c7800cc6cabeb75653ec34a2541afa26f5232d
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2022-06-16 09:00:31 +0200

    Add project files.

---
+++ LiveLearn20220615/Område.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveLearn20220615
+{
+    internal class Område
+    {
+        public string Navn { get; }
+        public int DanskeStemmer { get; set; }
+
+        public int TyskeStemmer { get; set; }
+
+        public Område(string navn)
+        {
+            Navn = navn;
+        }
+
+        public string Resultat()
+        {
+            if (DanskeStemmer > TyskeStemmer)
+            {
+                return "dansk";
+            } 
+            if (TyskeStemmer > DanskeStemmer)
+            {
+                return "tysk";
+            }
+            return "stemmelighed";
+        }
+    }
+}
---
+++ LiveLearn20220615/Program.cs
@@ -0,0 +1,41 @@
+﻿using LiveLearn20220615;
+
+Område[] stemmer = { new ("Område A"), new ("Område B"), new ("Område C"), new ("Område D"), new ("Område E") };
+
+int danskeTotal = 0;
+int tyskeTotal = 0;
+foreach(Område område in stemmer)
+{
+    Console.WriteLine($"Indtast antal danske stemmer i {område.Navn}");
+
+
+    int indtastetDansk = SikkerIndtastning();
+    område.DanskeStemmer = indtastetDansk;
+    danskeTotal += område.DanskeStemmer;
+
+    Console.WriteLine($"Indtast antal tyske stemmer i {område.Navn}");
+    område.TyskeStemmer = SikkerIndtastning();
+    tyskeTotal += område.TyskeStemmer;
+
+    Console.WriteLine(område.Resultat());
+}
+
+Console.WriteLine($"Danske i alt {danskeTotal}");
+Console.WriteLine($"Tyske i alt {tyskeTotal}");
+
+int SikkerIndtastning()
+{
+    
+    int indtastetTal;
+    bool varDetEtTal;
+    do
+    {
+        string? indtastetString = Console.ReadLine();
+        varDetEtTal = Int32.TryParse(indtastetString, out indtastetTal);
+        if (!varDetEtTal)
+        {
+            Console.WriteLine("Det skulle have været et tal");
+        }
+    } while (!varDetEtTal);
+    return indtastetTal;
+}
